3b6174d9bdfe17a907254dd89811b453ee5fea1e
Chế Lan Hải <114587471+Hai-Che@users.noreply.github.com>
2024-11-12 13:22:44 +0700

    Time: 0 ms (100%), Space: 48.7 MB (88.88%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    const result = []
+    function traverse(root) {
+        if (!root) return null;
+        traverse(root.left)
+        traverse(root.right)
+        result.push(root.val)
+    }
+    traverse(root)
+    return result
+};
