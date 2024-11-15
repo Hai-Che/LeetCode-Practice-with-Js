1862af8911452da87a82585847b2a293c9170e5d
Chế Lan Hải <114587471+Hai-Che@users.noreply.github.com>
2024-11-15 15:08:12 +0700

    Time: 7 ms (73.48%), Space: 54.1 MB (84.9%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+    let left = 0;
+    let right = s.length - 1
+    let arr = s.split('')
+    while (left < right) {
+        while (left < right && !vowels.has(arr[left])) left++;
+        while (left < right && !vowels.has(arr[right])) right--;
+        [arr[left], arr[right]] = [arr[right], arr[left]]
+        left++;
+        right--;
+    }
+    return arr.join('')
+};
